09b749bfce7cd8b915e4105969b0ede4fbbbc6f8
Nam <namjjang6@gmail.com>
2015-10-05 10:09:50 +0900

    v0.03 Constatnts 배열삭제, enum으로 전체 변경

--- src/GEConstants.java
+++ src/GEConstants.java
@@ -1,7 +1,64 @@
 public class GEConstants {
-	public static enum menuNames {File, Edit, Color};
-	public static enum toolBarButtonNames {oval, rectangle, line, polygon, heart, text};
-	public final static String FileMenuItems[] = {"New", "Open", "Save", "Save_As", "Exit"};
-	public final static String EditMenuItems[] = {"Undo", "Redo", "Cut", "Copy", "Paste", "Delete", "Select_all", "Deselect_all"};
-	public final static String ColorMenuItems[] = {"Line_color", "Fill_color"};
+	public static enum Menus {
+		File("File", GEConstants.FileMenuItems.values()), 
+		Edit("Edit", GEConstants.EditMenuItems.values()), 
+		Color("Color", GEConstants.ColorMenuItems.values());
+		
+		private String name;
+		private Enum[] items;
+		private Menus(String s, Enum[] e){
+			this.name = s;
+			this.items = e;
+		}
+		public String getName(){
+			return this.name;
+		}
+		public Enum[] getItems(){
+			return this.items;
+		}
+	};
+	
+	public static enum ToolBars {
+		Oval("oval"), Rectangle("rectangle"), Line("line"), Polygon("polygon"), Heart("heart"), Text("text");
+		private String name;
+		private ToolBars(String s){
+			this.name = s;
+		}
+		public String getName(){
+			return this.name;
+		}
+	};
+	
+	public static enum FileMenuItems{
+		New("New"), Open("Open"), Save("Save"), Save_As("Save_As"), Exit("Exit");
+		private String name;
+		private FileMenuItems(String s){
+			this.name = s;
+		}
+		public String getName(){
+			return this.name;
+		}
+	};
+	
+	public static enum EditMenuItems{
+		Undo("Undo"), Redo("Redo"), Cut("Cut"), Copy("Copy"), Paste("Paste"), Delete("Delete"), Select_all("Select_all"), Deselect_all("Deselect_all");
+		private String name;
+		private EditMenuItems(String s){
+			this.name = s;
+		}
+		public String getName(){
+			return this.name;
+		}
+	};
+	
+	public static enum ColorMenuItems{
+		Line_color("Line_color"), Fill_color("Fill_color");
+		private String name;
+		private ColorMenuItems(String s){
+			this.name = s;
+		}
+		public String getName(){
+			return this.name;
+		}
+	};
 }
--- src/GEToolBar.java
+++ src/GEToolBar.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.lang.reflect.Constructor;
 import java.util.Vector;
 
 import javax.swing.ButtonGroup;
@@ -16,27 +15,16 @@ public class GEToolBar extends JToolBar{
 		VectorButtons = new Vector<JRadioButton>();
 		ButtonGroup buttonGroup = new ButtonGroup();
 		
-		Class cls;
-		Constructor ct;
-		Object[] constructorParamObject;
-		Class[] constructorParamClass = new Class[] {String.class};
 		int i = 0;
-		try {
-			// reflection, enum, vector를 이용한 동적 툴바 버튼 생성
-			for(GEConstants.toolBarButtonNames name : GEConstants.toolBarButtonNames.values()){
-				cls = Class.forName("javax.swing.JRadioButton");
-				constructorParamObject = new Object[] {name.toString()};
-				VectorButtons.add((JRadioButton)cls.newInstance());	// 새로운 객체 생성
-				VectorButtons.get(i).setIcon(new ImageIcon("rsc/" + name + ".png"));
-				VectorButtons.get(i).setSelectedIcon(new ImageIcon("rsc/" + name + "_click.png"));
-				VectorButtons.get(i).setBackground(Color.decode("#fafbfc"));
-				this.add(VectorButtons.get(i));		// 메뉴바에 메뉴 항목 추가
-				buttonGroup.add(VectorButtons.get(i));	// 버튼 그룹 항목으로 등록
-				i++;
-			}
-		} catch (ClassNotFoundException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+		for(GEConstants.ToolBars name : GEConstants.ToolBars.values()){				
+			VectorButtons.add(new JRadioButton());
+			VectorButtons.get(i).setIcon(new ImageIcon("rsc/" + name + ".png"));
+			VectorButtons.get(i).setSelectedIcon(new ImageIcon("rsc/" + name + "_click.png"));
+			VectorButtons.get(i).setBackground(Color.decode("#fafbfc"));
+			this.add(VectorButtons.get(i));		// 메뉴바에 메뉴 항목 추가
+			buttonGroup.add(VectorButtons.get(i));	// 버튼 그룹 항목으로 등록
+			i++;
 		}
 	}
 }
